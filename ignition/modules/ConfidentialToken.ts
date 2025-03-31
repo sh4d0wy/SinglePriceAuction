13275c94a93152344c315e01e608d17f951830b3
DevSwayam <swayamkarle_ce_2020@ltce.in>
2025-04-01 04:32:33 +0530

    Changed Variable Name

--- ignition/modules/ConfidentialToken.ts
+++ ignition/modules/ConfidentialToken.ts
@@ -3,9 +3,9 @@
 
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const ConfidentialityTokenModule = buildModule("ConfidentialityTokenModule", (m) => {
-  const twoOfThreeGame = m.contract("ConfidentialERC20");
-  return { twoOfThreeGame };
+const ConfidentialERC20Module = buildModule("ConfidentialERC20Module", (m) => {
+  const confidentialERC20Module = m.contract("ConfidentialERC20");
+  return { confidentialERC20Module };
 });
 
-export default ConfidentialityTokenModule;
+export default ConfidentialERC20Module;
